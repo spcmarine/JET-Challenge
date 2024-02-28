b7c82415d86f9619d2600ca11749fa61875d0dd9
Dan Gibson <spcmarine@btinternet.com>
2024-02-28 21:30:09 +0000

    formatted in the console

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const postcode = 'RH121EP'
+const postcode = 'RH121EP';
 
 const fetchData = (postcode) => {
     fetch(`https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`)
     .then(response => response.json())
     .then(data => {
-        const firstTenResturants = data.restaurants.slice(0, 10)
-        console.log(firstTenResturants)
+        const firstTenResturants = data.restaurants.slice(0, 10);
+        console.log(firstTenResturants);
 
         firstTenResturants.forEach(restaurant => {
-            // console.log(restaurant.name)
+            console.log('Name: ' + restaurant.name);
             restaurant.cuisines.slice(0, 2).forEach(cuisine => {
-                console.log(cuisine.name)
-            })
-            console.log(restaurant.rating.starRating)
-            console.log(restaurant.address.firstLine + ' ' + restaurant.address.postalCode)
-
+                console.log('Cuisine: ' + cuisine.name);
+            });
+            console.log('Rating: ' + restaurant.rating.starRating);
+            console.log('Address: ' + restaurant.address.firstLine + ' ' + restaurant.address.postalCode);
+            console.log('')
 
         });
         
         
-    })
-}
+    });
+};
 
-fetchData(postcode)
+fetchData(postcode);
