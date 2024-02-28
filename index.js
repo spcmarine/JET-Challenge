2b370d75fecd29140371f376ab29b3d4a6cf1a37
Dan Gibson <spcmarine@btinternet.com>
2024-02-28 21:10:48 +0000

    logging out key data

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const fetchData = (postcode) => {
     fetch(`https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`)
     .then(response => response.json())
     .then(data => {
-        // console.log(data.restaurants)
         const firstTenResturants = data.restaurants.slice(0, 10)
         console.log(firstTenResturants)
+
+        firstTenResturants.forEach(restaurant => {
+            // console.log(restaurant.name)
+            
+            // console.log(restaurant.rating.starRating)
+            // console.log(restaurant.address.firstLine + ' ' + restaurant.address.postalCode)
+
+
+        });
+        
         
     })
 }
