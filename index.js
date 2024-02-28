c551bca82538559e439f57916cffcebac2728894
Dan Gibson <spcmarine@btinternet.com>
2024-02-28 20:57:06 +0000

    filtered first 10 restaurants

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const fetchData = (postcode) => {
     fetch(`https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        // console.log(data.restaurants)
+        const firstTenResturants = data.restaurants.slice(0, 10)
+        console.log(firstTenResturants)
+        
     })
 }
 
