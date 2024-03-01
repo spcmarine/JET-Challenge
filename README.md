68c1f14f176cb470b44af6f2458a568637ee29bd
Dan Gibson <spcmarine@btinternet.com>
2024-03-01 20:06:17 +0000

    create DOM elements

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ● Add how to build, compile and run your solution into the README
 ● Include any assumptions or things that were not clear to you in the README
 ● Include any improvements you’d make to your solution in the README
+
+
+- in the cuisines array there are objects that I didn't consider to be cuisines. I would ordinarily seek clarifications over if these are to be displayed as per the brief. 
--- index.js
+++ index.js
@@ -7,19 +7,32 @@ const fetchData = (postcode) => {
         const firstTenResturants = data.restaurants.slice(0, 10);
         console.log(firstTenResturants);
 
-        firstTenResturants.forEach(restaurant => {
-            console.log('Name: ' + restaurant.name);
-            restaurant.cuisines.slice(0, 2).forEach(cuisine => {
-                console.log('Cuisine: ' + cuisine.name);
-            });
-            console.log('Rating: ' + restaurant.rating.starRating);
-            console.log('Address: ' + restaurant.address.firstLine + ' ' + restaurant.address.postalCode);
-            console.log('')
+        const mainContainer = document.querySelector('.main-container');;
 
-        });
-        
-        
+        firstTenResturants.forEach(restaurant => {
+            const restaurantDiv = document.createElement('div');
+            restaurantDiv.classList.add('restaurant');
+
+            const nameElement = document.createElement('p');
+            nameElement.textContent = 'Name: ' + restaurant.name;
+            restaurantDiv.appendChild(nameElement);
+
+            const cuisinesElement = document.createElement('p');
+            cuisinesElement.textContent = 'Cuisine: ' + restaurant.cuisines.slice(0, 2).map(cuisine => cuisine.name).join(', ');
+            restaurantDiv.appendChild(cuisinesElement);
+
+            const ratingElement = document.createElement('p');
+            ratingElement.textContent = 'Rating: ' + restaurant.rating.starRating;
+            restaurantDiv.appendChild(ratingElement);
+
+            const addressElement = document.createElement('p');
+            addressElement.textContent = 'Address: ' + restaurant.address.firstLine + ' ' + restaurant.address.postalCode;
+            restaurantDiv.appendChild(addressElement);
+
+            mainContainer.appendChild(restaurantDiv);
+        }); 
     });
 };
+
 
 fetchData(postcode);
