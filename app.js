0f49a05cf5e60aa70a36e6cf34e7ef2dd039b851
Dan Gibson <spcmarine@btinternet.com>
2024-03-02 13:25:03 +0000

    debugging CORS

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/restaurants/:postcode', (req, res) => {
+    const { postcode } = req.params; 
+    
+    fetch(`https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch data from the API');
+        }
+        return response.json();
+    })
+    .then(data => {
+        res.json(data); 
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Error fetching data from the API');
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const postcode = 'RH121EP';
 
 const fetchData = (postcode) => {
-    fetch(`https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`)
+    fetch(`localhost/3000/${postcode}`)
     .then(response => response.json())
     .then(data => {
         const firstTenResturants = data.restaurants.slice(0, 10);
