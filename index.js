1a5690da731c103424b2dd3e3c5fb1980c167ab0
Dan Gibson <spcmarine@btinternet.com>
2024-03-02 15:11:17 +0000

    MVP met

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const fetchData = (postcode) => {
     .then(data => {
         const firstTenResturants = data.restaurants.slice(0, 10);
 
-        const mainContainer = document.querySelector('.main-container');;
+        const mainContainer = document.querySelector('.main-container');
 
         firstTenResturants.forEach(restaurant => {
             const restaurantDiv = document.createElement('div');
             restaurantDiv.classList.add('restaurant');
 
             const nameElement = document.createElement('p');
+            console.log(restaurant.name)
             nameElement.classList.add('name');
             nameElement.textContent = 'Name: ' + restaurant.name;
             restaurantDiv.appendChild(nameElement);
+
 
             const cuisinesElement = document.createElement('p');
             cuisinesElement.classList.add('cuisine');
